913499e974360c7859d4f0a83105005fcf3d4587
Alex Feigin <afeigin@proofpoint.com>
2025-05-06 16:49:57 +0300

    cleanup

--- profile.d/quicksetup.sh
+++ profile.d/quicksetup.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
 curl -fsSL https://raw.githubusercontent.com/alexfeigin/terminal-talk/refs/heads/master/profile.d/bash_profile.sh -o ~/.bash_profile
-
-brew install coreutils jq awscli vault fzf flock bat fd btop tmux git tlrc bash-completion
-
-sudo mkdir -p /usr/local/etc/profile.d
-for file in awshelper.sh calcpath.sh curlutils.sh fzf.sh generate_secure_password.sh idea.sh jdk.sh json_helpers.sh kubectlcomplete.sh passcopy.sh sources.sh unzip.sh; do
-  sudo curl -fsSL "https://raw.githubusercontent.com/alexfeigin/terminal-talk/refs/heads/master/profile.d/${file}" -o "/usr/local/etc/profile.d/${file}"
-done
-
 cat <<EOF > ~/.bashrc
 if [ -f ~/.bash_profile ]; then
 . ~/.bash_profile
 fi
 EOF
 
+if ! command -v brew &> /dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+curl -fsSL "https://raw.githubusercontent.com/alexfeigin/terminal-talk/refs/heads/master/Brewfile" -o Brewfile
+brew tap homebrew/bundle
+brew bundle Brewfile
+
+sudo mkdir -p /usr/local/etc/profile.d
+for file in awshelper.sh calcpath.sh curlutils.sh fzf.sh generate_secure_password.sh idea.sh jdk.sh json_helpers.sh kubectlcomplete.sh passcopy.sh sources.sh unzip.sh; do
+  sudo curl -fsSL "https://raw.githubusercontent.com/alexfeigin/terminal-talk/refs/heads/master/profile.d/${file}" -o "${file}"
+done
